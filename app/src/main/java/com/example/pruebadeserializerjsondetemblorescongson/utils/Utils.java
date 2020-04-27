5e5933583f5977f8f5e0437ad56ade83c4eb01f8
GillesVN <mmmartinezr1003@alumno.ipn.mx>
2020-04-27 15:41:11 -0500

    Se añade el modo oscuro usando el forceDark de la documentación

--- app/src/main/java/com/example/pruebadeserializerjsondetemblorescongson/utils/Utils.java
+++ app/src/main/java/com/example/pruebadeserializerjsondetemblorescongson/utils/Utils.java
@@ -4,7 +4,7 @@ import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.SharedPreferences;
-import android.support.v4.content.ContextCompat;
+import androidx.core.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 
